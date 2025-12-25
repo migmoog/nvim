fb79eca1f36aeb37df0a91c11543d0464148e23e
Jeremy Gordon <jgordon@pop-os.home>
2025-12-25 14:35:24 -0500

    Warning diagnostics

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -12,6 +12,16 @@ return {
 			"<leader>e",
 			"<cmd>Trouble diagnostics toggle filter = { buf=0, severity=vim.diagnostic.severity.ERROR }<cr>",
 			desc = "[e]rror diagnostics for this buffer"
+		},
+		{
+			"<leader>W",
+			"<cmd>Trouble diagnostics toggle filter.severity=vim.diagnostic.severity.WARNING<cr>",
+			desc = "[W]arning diagnostics for the entire workspace"
+		},
+		{
+			"<leader>w",
+			"<cmd>Trouble diagnostics toggle filter = { buf=0, severity=vim.diagnostic.severity.WARNING }<cr>",
+			desc = "[w]arning diagnostics for this buffer"
 		}
 	},
 }
