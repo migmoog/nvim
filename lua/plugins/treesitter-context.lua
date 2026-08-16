144899f45dbac256660b03e66f37021ed5364309
Jeremy <me@jeremygordon.dev>
2026-08-15 22:25:11 -0500

    bold sticky scroll tag

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -1,7 +1,14 @@
 return {
 	"nvim-treesitter/nvim-treesitter-context",
 	config = function ()
-		require("treesitter-context").setup {}
+		require("treesitter-context").setup {
+			multiwindow = true,
+		}
+
+		vim.api.nvim_set_hl(0, "TreesitterContext", {
+			bold = true,
+			update = true,
+		})
 
 		vim.keymap.set("n", "<leader>tC", ":TSContext toggle<CR>", {
 			desc = "[T]oggle [C]ontext",
