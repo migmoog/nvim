6b50bfb10ecf93c7e9fe454c9df46bff3f114a58
Jeremy <me@jeremygordon.dev>
2026-09-11 16:30:04 -0400

    Autocomplete braces in VimTeX

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -2,6 +2,9 @@ return {
   "lervag/vimtex",
   lazy = false,
   init = function()
+    -- should install w/ "sudo pacman -S zathura zathura-mupdf-pdf"
     vim.g.vimtex_view_method = "zathura"
+    -- enables braces autocomplete 
+    vim.g.vimtex_complete_close_braces = 1
   end
 }
